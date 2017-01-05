dbbab900999a5ba664465500aa9e25a1f2e0b42d
lizhonghui <lzhspace@gmail.com>
2017-01-05 21:45:01 +0800

    fix: update polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,13 +1,7 @@
 // Polyfills
-// These modules are what's in angular 2 bundle polyfills so don't include them
-// import 'es6-shim';
-// import 'es6-promise';
-// import 'reflect-metadata';
 
-// CoreJS has all the polyfills you need
+import 'core-js'; // include reflect
+import 'zone.js/dist/zone';
 
-// import 'core-js';
-import 'zone.js';
-import 'reflect-metadata';
-// require('zone.js/dist/zone');
-// require('zone.js/dist/long-stack-trace-zone');
+// give more meaningful stack traces in the development environment
+import 'zone.js/dist/long-stack-trace-zone'; 
